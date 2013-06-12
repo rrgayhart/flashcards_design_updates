5234e491823bf39a0f2d241dc95ed5e5f4956585
Meeka Gayhart <rrgayhart@gmail.com>
2013-06-12 17:50:51 -0400

    Minute 11 in tutorial

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,29 @@
 class CardsController < ApplicationController
 	def show
-		@deck = Deck.find(params[:deck_id])
+		@deck = find_deck
 		@card = @deck.cards.find(params[:id])
+	end
+
+	def new
+		@deck = find_deck
+		@card = @deck.cards.new
+	end
+
+	def create
+		@deck = find_deck
+		@card = @deck.cards.new(params[:card])
+		@card.save
+		redirect_to @deck
+	end
+
+	def edit
+		@deck = find_deck
+		@card = @deck.cards.find(params[:id])
+	end
+	
+private
+
+	def find_deck
+		Deck.find(params[:deck_id])
 	end
 end
